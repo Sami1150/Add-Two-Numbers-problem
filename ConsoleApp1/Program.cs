693588177054dd9dad1a6db9fc3ad52c10e936f9
Sami1150 <bsef19m045@pucit.edu.pk>
2023-01-18 03:49:37 +0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int firstNum; int secondNum = 0;
+        int sum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            firstNum = nums[i];
+            for (int j = 0; j < nums.Length; j++)
+            {
+                secondNum = nums[j];
+                if (i != j)
+                {
+                    sum = firstNum + secondNum;
+                    if (sum == target)
+                    {
+                        int[] numbers = { i, j };
+                        return numbers;
+                    }
+                }
+
+            }
+        }
+        return Array.Empty<int>();
+    }
+}
